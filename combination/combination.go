21adf4248fa24641bfe14666f43e5e16a7387309
xzf <zhifa.xiang@enmotech.com>
2024-10-10 19:29:27 +0800

    combination

---
+++ combination/combination.go
@@ -0,0 +1,51 @@
+package combination
+
+func GetAllIndexArray(all int, pick int) [][]int {
+	return getAllIndexArray(getAllIndexArrayReq{})
+}
+
+type getAllIndexArrayReq struct {
+	in   []int
+	all  int
+	pick int
+}
+
+func getAllIndexArray(req getAllIndexArrayReq) (out [][]int) {
+	nextPick := getAllNextPick(req.all, []int{})
+	if len(nextPick) == req.all-req.pick {
+		return [][]int{req.in}
+	}
+	for _, item := range nextPick {
+		out = append(out,
+			getAllIndexArray(getAllIndexArrayReq{
+				in:   append(append([]int{}, req.in...), item),
+				all:  req.all,
+				pick: req.pick,
+			})...)
+	}
+	return
+}
+
+func getAllNextPick(all int, in []int) (out []int) {
+	if len(in) == 0 {
+		for i := 1; i <= all; i++ {
+			out = append(out, i)
+		}
+		return
+	}
+	if len(in) == all {
+		return
+	}
+	have := map[int]struct{}{}
+	for i := 0; i < len(in); i++ {
+		have[in[i]] = struct{}{}
+	}
+	for item := 1; item <= all; item++ {
+		_, ok := have[item]
+		if ok {
+			continue
+		}
+		out = append(out, item)
+	}
+	return
+}
